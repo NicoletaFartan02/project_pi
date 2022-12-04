d7db792f23e5c0447f24aef4a1f287a9c171d000
nicoletaftn <nicoleta.fartan02@e-uvt.ro>
2022-12-04 19:50:10 +0200

    new
    changes

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+# to activate the app in cmd ".\\env\Scripts\activate"
+# to run the app "flask run"
+
+import json
 import requests
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
@@ -17,7 +21,10 @@ def get_weather_data(city):
     url = f'https://api.openweathermap.org/data/2.5/weather?q={ city }&units=metric&appid=d5ed97edccf5d24a177d9c380f32403b'
     r = requests.get(url).json()
     return r
-
+def get_weather_data_5(city):
+    url = f'https://api.openweathermap.org/data/2.5/forecast?q={ city }&units=metric&appid=d5ed97edccf5d24a177d9c380f32403b'
+    r1 = requests.get(url).json()
+    return r1
 @app.route('/')
 def index_get():
    
@@ -31,6 +38,10 @@ def index_get():
         r = get_weather_data(city.name)  
 
         print(r)
+
+        r1 = get_weather_data_5(city.name)
+        print("##############")
+        print(r1)
 
         weather = {
             'city' : city.name,
@@ -77,3 +88,52 @@ def delete_city(name):
     flash(f'Orasul { city.name } sters cu succes!', 'success')
 
     return redirect(url_for('index_get'))
+
+@app.route('/chartjs/')
+def chartjs():
+    #return render_template('chartjs.html')
+    new_city = request.form.get('city')
+
+    if new_city:
+        existing_city = City.query.filter_by(name=new_city).first()
+        if not existing_city:
+            new_city_data = get_weather_data_5(new_city)
+            if new_city_data['cod'] == 200:
+                new_city_obj = City(name=new_city)
+                db.session.add(new_city_obj)
+                db.session.commit()
+
+    cities = City.query.all()
+     
+    weather_data1 = []
+    weather_data2 = []
+
+    for city in cities:
+
+        r1 = get_weather_data_5(city.name)  
+
+        print(r1)
+
+        weather_days = {
+            'day1' : r1['list'][0]['dt_txt'],
+            'day2' : r1['list'][1]['dt_txt'],
+            'day3' : r1['list'][2]['dt_txt'],
+            'day4' : r1['list'][3]['dt_txt'],
+            'day5' : r1['list'][4]['dt_txt'],
+                
+        }
+        weather_temp = {
+
+            'temperature1' : r1['list'][0]['main']['temp'],
+            'temperature2' : r1['list'][1]['main']['temp'],
+            'temperature3' : r1['list'][2]['main']['temp'],
+            'temperature4' : r1['list'][3]['main']['temp'],
+            'temperature5' : r1['list'][4]['main']['temp'],
+        }
+    labels = [row[1] for row in weather_days]
+    values = [row[1] for row in weather_temp]
+
+       # weather_data1.append(weather_days)
+        #weather_data2.append(weather_temp)
+   # return render_template('chartjs.html', weather_data1 = json.dumps(weather_days),  weather_data2 = json.dumps(weather_temp))
+    return render_template('chartjs.html', labels=weather_days, values=weather_temp)
